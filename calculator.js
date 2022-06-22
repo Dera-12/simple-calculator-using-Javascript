dd3413fb71998225019df5a6be82b1630292b391
chidera Jessica <jescachidera@gmail.com>
2022-06-23 00:22:36 +0100

    initial commit

---
+++ calculator.js
@@ -0,0 +1,23 @@
+let a,b, c,add, sub,mul, div;
+a=parseInt(prompt("Enter First Number"));
+b=parseInt(prompt("Enter Second Number"));
+
+c=parseInt(prompt("Choose all the arithemitic operators + - / *"));
+// choose all the operators//
+
+add=a+b;
+// addition//
+alert("add is" +add);
+sub=a-b;
+// subtraction //
+
+alert("sub is" +sub);
+mul=a*b;
+// mutiplication//
+alert("mul is" +mul);
+div=a/b;
+//division //
+alert("div is" +div);
+
+             
+
